449d5e9f889743470ec1c6a8bad2a9e17f5490f0
Sarthak <sarthakdhakde@gmail.com>
2025-08-28 15:08:34 +0530

    Added client and server workflows for Github Actions

---
+++ server/src/test/java/com/example/podify/PodifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.podify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
